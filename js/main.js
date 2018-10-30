1f309bdec795bfc9ca883409671763bcb4ff58fa
Shaikat <shaikat0509@gmail.com>
2018-10-31 01:47:33 +0600

    Updates 31/10/2018

--- js/main.js
+++ js/main.js
@@ -14,16 +14,40 @@ jQuery(document).ready(function($) {
     $('body').removeClass('showing-section');
   });
 
-
-
-
-
-
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
+  var $container = $('.isotope-items'),
+    $filterLinks = $('.filter-nav li a');
 
+  $container.isotope({
+    itemSelector: '.item',
+    filter: '.item'
+  });
+  $filterLinks.click(function(e){
+    e.preventDefault();
+    var $this = $(this);
+    if ( $this.hasClass('selected') ) {
+      return;
+    }
+    $filterLinks.filter('.selected').removeClass('selected');
+    $this.addClass('selected');
+    selector = $this.data('filter');
+    $container.isotope({
+      filter: selector
+    });
+  });
+
+  if($(window).width() < 768) {
+    var selected_item = $('.filter-nav .selected .item-cat').text();
+    $('.isotope-nav').append('<span>' + selected_item + '</span>');
+    $('.isotope-nav').click(function() {
+      $('.filter-nav').slideToggle();
+      var selected_item = $('.filter-nav .selected .item-cat').text();
+      $('.isotope-nav > span').text(selected_item);
+    });
+  };
 
 
 
